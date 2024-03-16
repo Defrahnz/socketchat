47df0f1d993f327ffd7c1fb105429895ebbcffa4
Daniel Valdivia <dvaldivia98@hotmail.com>
2024-03-15 21:32:16 -0600

    Se agregó el código del service para que pueda funcionar la conexión al socket

--- ng-chat-socket/src/app/services/chat.service.ts
+++ ng-chat-socket/src/app/services/chat.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
-
+import * as SockJS from 'sockjs-client';
+import {Stomp} from  '@stomp/stompjs';
+import { ChatMessage } from '../models/chat-message';
 @Injectable({
   providedIn: 'root'
 })
 export class ChatService {
 
-  constructor() { }
+  private stompClient: any
+  constructor() { 
+    this.initConectionSocket();
+  }
+
+  initConectionSocket(){
+    /* const url= "http://localhost:3000/chat-socket"; */
+    const url= "chat-socket/";
+    const socket= new SockJS(url)
+    this.stompClient = Stomp.over(socket);
+  }
+
+  joinRoom(roomId:String){
+    this.stompClient.connect({},()=>{
+      this.stompClient.subscribe(`/topic/${roomId}`,(messages:any)=>{
+        const messageContent=JSON.parse(messages.body);
+        console.log(messageContent);
+      })
+    })
+  }
+  sendMessage(roomId:String, ChatMessage: ChatMessage){
+      this.stompClient.send(`/app/chat/${roomId}`,{},JSON.stringify(ChatMessage));
+  }
 }
